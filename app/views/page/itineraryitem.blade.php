613daf69b478198987c2a9d880d2e5f0eab71ead
gidj02 <ginodejesus02@gmail.com>
2015-04-12 04:09:52 +0800

    looping in items

--- app/views/page/itineraryitem.blade.php
+++ app/views/page/itineraryitem.blade.php
@@ -8,6 +8,9 @@
 	        <h1 id="timeline"><small>Welcome to your </small><br>{{ $itinerary->name }} </h1>
 	    </div>
 	    <ul class="timeline">
+	    <?php $x = 0; ?>
+	    @foreach($item as $value)
+	    	@if ($x%2 == 0)
 	        <li>
 	          <div class="timeline-badge primary"><a><i class="glyphicon glyphicon-record" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
 	          <div class="timeline-panel">
@@ -16,19 +19,22 @@
 
 	            </div>
 	            <div class="timeline-body">
-	              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+
+	              <p>{{ $value->blog }}</p>
 
 	            </div>
 
 	            <div class="timeline-footer">
 	                <a><i class="glyphicon glyphicon-thumbs-up"></i></a>
-	                <a><i class="glyphicon glyphicon-share"></i></a>
-	                <a class="pull-right">Continuar Lendo</a>
+	                <a><h4>{{ $value->name }}</h4></a>
+	                <a class="pull-right">Continue Reading	</a>
 	            </div>
 	          </div>
 	        </li>
+	        @endif
 
-	        <li  class="timeline-inverted">
+	        @if ($x%2 == 1)
+	         <li  class="timeline-inverted">
 	          <div class="timeline-badge primary"><a><i class="glyphicon glyphicon-record invert" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
 	          <div class="timeline-panel">
 	            <div class="timeline-heading">
@@ -36,106 +42,22 @@
 
 	            </div>
 	            <div class="timeline-body">
-	              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+	              <p>{{ $value->blog }}</p>
 
 	            </div>
 
 	            <div class="timeline-footer">
 	                <a><i class="glyphicon glyphicon-thumbs-up"></i></a>
-	                <a><i class="glyphicon glyphicon-share"></i></a>
-	                <a class="pull-right">Continuar Lendo</a>
+	                <a><h4>{{ $value->name }}</h4></a>
+	                <a class="pull-right">Continue Reading</a>
 	            </div>
 	          </div>
 	        </li>
-	        <li>
-	          <div class="timeline-badge primary"><a><i class="glyphicon glyphicon-record" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
-	          <div class="timeline-panel">
-	            <div class="timeline-heading">
-	              <img class="img-responsive" src="http://lorempixel.com/1600/500/sports/2" />
-
-	            </div>
-	            <div class="timeline-body">
-	              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-
-	            </div>
-
-	            <div class="timeline-footer">
-	                <a><i class="glyphicon glyphicon-thumbs-up"></i></a>
-	                <a><i class="glyphicon glyphicon-share"></i></a>
-	                <a class="pull-right">Continuar Lendo</a>
-	            </div>
-	          </div>
-	        </li>
-
-	        <li  class="timeline-inverted">
-	          <div class="timeline-badge primary"><a><i class="glyphicon glyphicon-record invert" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
-	          <div class="timeline-panel">
-	            <div class="timeline-body">
-	              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-
-	            </div>
-
-	            <div class="timeline-footer">
-	                <a><i class="glyphicon glyphicon-thumbs-up"></i></a>
-	                <a><i class="glyphicon glyphicon-share"></i></a>
-	                <a class="pull-right">Continuar Lendo</a>
-	            </div>
-	          </div>
-	        </li>
-	        <li>
-	          <div class="timeline-badge primary"><a><i class="glyphicon glyphicon-record" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
-	          <div class="timeline-panel">
-	            <div class="timeline-heading">
-	              <img class="img-responsive" src="http://lorempixel.com/1600/500/sports/2" />
-
-	            </div>
-	            <div class="timeline-body">
-	              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-
-	            </div>
-
-	            <div class="timeline-footer">
-	                <a><i class="glyphicon glyphicon-thumbs-up"></i></a>
-	                <a><i class="glyphicon glyphicon-share"></i></a>
-	                <a class="pull-right">Continuar Lendo</a>
-	            </div>
-	          </div>
-	        </li>
-
-	        <li  class="timeline-inverted">
-	          <div class="timeline-badge primary"><a><i class="glyphicon glyphicon-record invert" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
-	          <div class="timeline-panel">
-	            <div class="timeline-heading">
-	              <img class="img-responsive" src="http://lorempixel.com/1600/500/sports/2" />
-
-	            </div>
-	            <div class="timeline-body">
-	              <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-
-	            </div>
-
-	            <div class="timeline-footer primary">
-	                <a><i class="glyphicon glyphicon-thumbs-up"></i></a>
-	                <a><i class="glyphicon glyphicon-share"></i></a>
-	                <a class="pull-right">Continuar Lendo</a>
-	            </div>
-	          </div>
-	        </li>
-	        <li>
-	          <div class="timeline-badge primary"><a><i class="glyphicon glyphicon-record invert" rel="tooltip" title="11 hours ago via Twitter" id=""></i></a></div>
-	          <div class="timeline-panel">
-	            <div class="timeline-body">
-	              <p><b>All the credits go to <a href="http://bootsnipp.com/rafamaciel">Rafamaciel</a></b></p>
-	              <p>I only make it responsive and remove the empty spaces to be more like Facebook timeline!</p>
-	            </div>
-
-	            <div class="timeline-footer primary">
-	                <a><i class="glyphicon glyphicon-thumbs-up"></i></a>
-	                <a><i class="glyphicon glyphicon-share"></i></a>
-	                <a class="pull-right">Continuar Lendo</a>
-	            </div>
-	          </div>
-	        </li>
+	        @endif
+	        <?php $x = $x + 1; ?>
+	    @endforeach
+	       
+	         
 
 	        <li class="clearfix" style="float: none;"></li>
 	    </ul>
