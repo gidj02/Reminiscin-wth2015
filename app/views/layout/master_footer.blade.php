2cc69da07d17f9d5486286edeba85eff0e23c67c
gidj02 <ginodejesus02@gmail.com>
2015-04-11 17:19:40 +0800

    temp ui

---
+++ app/views/layout/master_footer.blade.php
@@ -0,0 +1,34 @@
+<!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                    <h4><strong>Foto.Op Prints</strong>
+                    </h4>
+                    <p>San Mateo<br>Rizal, Philippines</p>
+                    <ul class="list-unstyled">
+                        <li><i class="fa fa-phone fa-fw"></i> (+63) 939-822-9813</li>
+                        <li><i class="fa fa-envelope-o fa-fw"></i>  <a href="mailto:name@example.com">fotoop@gmail.com</a>
+                        </li>
+                    </ul>
+                    <br>
+                    <ul class="list-inline">
+                        <li>
+                            <a href="#"><i class="fa fa-facebook fa-fw fa-3x"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-twitter fa-fw fa-3x"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-google-plus fa-fw fa-3x"></i></a>
+                        </li>
+                    </ul>
+                    <hr class="small">
+                    <p class="text-muted">Copyright © Fotoop | Prints 2015</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+</html>
