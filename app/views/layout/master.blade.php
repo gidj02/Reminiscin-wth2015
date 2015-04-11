a1d8a59ce25a9974a4c61b60b49cba9546edf751
gidj02 <ginodejesus02@gmail.com>
2015-04-11 18:54:53 +0800

    Fix ui

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -1,4 +1,4 @@
-<!-- <html>
+<html>
 	<head>
 		<title> WTH2015 </title>
 		{{ HTML::style('bootstrap.css')}}
@@ -21,43 +21,4 @@
 			@yield('contents')
 		</div>
 	</body>
-</html> -->
 </html>
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>WTH2015 | Sentiment</title>
-
-    <!-- Bootstrap Core CSS -->
-    {{ HTML::style('css/bootstrap.min.css')}}
-    {{ HTML::style('css/bootstrap.css')}}
-
-    <!-- Custom CSS -->
-    {{ HTML::style('css/stylish-portfolio.css')}}
-
-    <!-- Custom Fonts -->
-    {{ HTML::style('font-awesome/css/font-awesome.min.css')}}
-    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-     <!-- jQuery -->
-    {{ HTML::script('js/jquery.js')}}
-
-    <!-- Bootstrap Core JavaScript -->
-    {{ HTML::script('js/bootstrap.min.js')}}
-
-</head>
-<body>
-
-
