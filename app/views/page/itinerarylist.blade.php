b1ce976ccdca4058a8a94b481c298ae9e07a226d
gidj02 <ginodejesus02@gmail.com>
2015-04-12 03:13:50 +0800

    Added foreach in itinerary list

--- app/views/page/itinerarylist.blade.php
+++ app/views/page/itinerarylist.blade.php
@@ -7,49 +7,20 @@
 </div>
 
 <div class="row">
+	@foreach($itinerary as $value)
 	<div class="col-lg-3 col-md-4 col-xs-6 thumb" id="itenerarylist">
 	    <a class="thumbnail" href="#">
 	        <img class="img-responsive" src="http://placehold.it/400x300" alt="">
 	    </a>
 	</div>
-
-	<div class="col-lg-3 col-md-4 col-xs-6 thumb" id="itenerarylist">
-	    <a class="thumbnail" href="#">
-	        <img class="img-responsive" src="http://placehold.it/400x300" alt="">
-	    </a>
-	</div>
-
-	<div class="col-lg-3 col-md-4 col-xs-6 thumb" id="itenerarylist">
-	    <a class="thumbnail" href="#">
-	        <img class="img-responsive" src="http://placehold.it/400x300" alt="">
-	    </a>
-	</div>
-
-	<div class="col-lg-3 col-md-4 col-xs-6 thumb" id="itenerarylist">
-	    <a class="thumbnail" href="#">
-	        <img class="img-responsive" src="http://placehold.it/400x300" alt="">
-	    </a>
-	</div>
-</div>
-
-<div class="row">
-	<div class="col-lg-3 col-md-4 col-xs-6 thumb" id="itenerarylist">
-	    <a class="thumbnail" href="#">
-	        <img class="img-responsive" src="http://placehold.it/400x300" alt="">
-	    </a>
-	</div>
-
-	<div class="col-lg-3 col-md-4 col-xs-6 thumb" id="itenerarylist">
-	    <a class="thumbnail" href="#">
-	        <img class="img-responsive" src="http://placehold.it/400x300" alt="">
-	    </a>
-	</div>
-
+	@endforeach
+	
 	<div class="col-lg-3 col-md-4 col-xs-6 thumb itemter">
 	    <a class="thumbnail" href="{{ URL::to('itinerary/create/' . $user->id) }}" id="additenerary">
 	        <span class="center glyphicon glyphicon-plus addter"></span>
 	    </a>
 	</div>
+
 </div>
 
 </div>
