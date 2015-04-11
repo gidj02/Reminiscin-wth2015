020ebc0dc91fd0b88a7ec01618751943a9a22a69
gidj02 <ginodejesus02@gmail.com>
2015-04-12 00:49:37 +0800

    Partial registration design

--- app/views/page/register.blade.php
+++ app/views/page/register.blade.php
@@ -1,68 +1,56 @@
 @extends('layout.master')
 
 @section('contents')
-	<div class="panel panel-primary">
-		<div class="panel-heading"><h1> Create User </h1></div>
-			<div class="panel-body">
-				{{ Form::open(['route' => 'user.store']) }}
-			<div class="row">
-		  		<div class="col-lg-12">
-		  			<div class="form-group">
-		                <label class="col-md-4 control-label">Username: </label>
-		                <div class="col-md-6">
-		              		{{ Form::text('username') }}
-		              		<br>
-							{{ $errors->first('username') }}
-		                </div>
-		            </div>
-					<br>
-					<div class="form-group">
-		                <label class="col-md-4 control-label">Password: </label>
-		                <div class="col-md-6">
-		               		{{ Form::password('password') }}
-		               		<br>	
+
+
+<div class='container' style='margin-top:30px; margin-bottom: 30px;'>	
+<div class='row'>
+    <div class='col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3'>
+		{{ Form::open(['route' => 'user.store']) }}
+			<h2>Please Sign Up <small>It's free and always will be.</small></h2>
+			<hr class='colorgraph'>
+				<div class='form-group'>
+					{{ Form::text('username','',array('class' => 'form-control input-md', 'placeholder' => 'Username', 'aria-describedby' => 'basic-addon1')) }}
+					{{ $errors->first('username') }}	
+				</div>
+				
+			
+				<div class='form-group'>
+					{{ Form::email('email','',array('class' => 'form-control input-md', 'placeholder' => 'Email Address', 'aria-describedby' => 'basic-addon1')) }}	
+					{{ $errors->first('email') }}
+				</div>
+
+				<div class='form-group'>
+					{{ Form::text('contact','',array('class' => 'form-control input-md', 'placeholder' => 'Contact Number', 'aria-describedby' => 'basic-addon1')) }}	
+					{{ $errors->first('contact') }}
+				</div>
+
+				<div class='row'>
+					<div class='col-xs-12 col-sm-6 col-md-6'>
+						<div class='form-group'>
+							{{ Form::password('password', array('class' => 'form-control input-md', 'placeholder' => 'Password', 'aria-describedby' => 'basic-addon1')) }}
+               				<br>	
 							{{ $errors->first('password') }}
-		                </div>
-		            </div>
-					<br>
-					<div class="form-group">
-		                <label class="col-md-4 control-label">Confirm Password: </label>
-		                <div class="col-md-6">
-		               		{{ Form::password('password_confirmation') }}	
-		               		<br>
-							{{ $errors->first('password_confirmation') }}
-		                </div>
-		            </div>
-					<br>
-					<div class="form-group">
-		                <label class="col-md-4 control-label">Email: </label>
-		                <div class="col-md-6">
-		               		{{ Form::email('email') }}	
-		               		<br>
-							{{ $errors->first('email') }}
-		                </div>
-		            </div>
-					<br>
-					<div class="form-group">
-		                <label class="col-md-4 control-label">Contact: </label>
-		                <div class="col-md-6">
-		               		{{ Form::text('contact') }}	
-		               		<br>
-							{{ $errors->first('contact') }}
-		                </div>
-		            </div>
-					<br>
-					<div class="form-group">
-		                <div class="col-md-6 col-md-offset-4">
-							{{ Form::submit('Create') }}
+						</div>
+					</div>
+					<div class='col-xs-12 col-sm-6 col-md-6'>
+						<div class='form-group'>
+							{{ Form::password('password',array('class' => 'form-control input-md', 'placeholder' => 'Confirm Password', 'aria-describedby' => 'basic-addon1')) }}
+               				<br>	
+							{{ $errors->first('password') }}
 						</div>
 					</div>
 				</div>
-			</div>
-			{{ Form::close() }}
-		</div>
-	</div>
+			
+			<hr class='colorgraph'>
+			<div class='row'>
 
-	
-	
+				<div class='col-xs-12 col-md-6'>
+					{{ Form::submit('Create') }}
+				</div>
+			</div>
+		{{ Form::close() }}
+	</div>
+</div>
+
 @stop
