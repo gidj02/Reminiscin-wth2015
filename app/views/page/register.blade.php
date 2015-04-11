d26173eb1b6e51dc28639127f1ee230049cfa0d0
gidj02 <ginodejesus02@gmail.com>
2015-04-12 00:56:38 +0800

    Finish registration design

--- app/views/page/register.blade.php
+++ app/views/page/register.blade.php
@@ -6,8 +6,11 @@
 <div class='container' style='margin-top:30px; margin-bottom: 30px;'>	
 <div class='row'>
     <div class='col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3'>
+    <div class='login-panel panel panel-default'>
 		{{ Form::open(['route' => 'user.store']) }}
-			<h2>Please Sign Up <small>It's free and always will be.</small></h2>
+		<div class='panel-heading'><h2>Please Sign Up <br><small>Make your memories last forever.</small></h2></div>
+		<div class='panel-body'>
+			
 			<hr class='colorgraph'>
 				<div class='form-group'>
 					{{ Form::text('username','',array('class' => 'form-control input-md', 'placeholder' => 'Username', 'aria-describedby' => 'basic-addon1')) }}
@@ -18,6 +21,21 @@
 				<div class='form-group'>
 					{{ Form::email('email','',array('class' => 'form-control input-md', 'placeholder' => 'Email Address', 'aria-describedby' => 'basic-addon1')) }}	
 					{{ $errors->first('email') }}
+				</div>
+
+				<div class='form-group'>
+					{{ Form::text('firstname','',array('class' => 'form-control input-md', 'placeholder' => 'First Name', 'aria-describedby' => 'basic-addon1')) }}	
+					{{ $errors->first('firstname') }}
+				</div>
+
+				<div class='form-group'>
+					{{ Form::text('middlename','',array('class' => 'form-control input-md', 'placeholder' => 'Middle Name', 'aria-describedby' => 'basic-addon1')) }}	
+					{{ $errors->first('middlename') }}
+				</div>
+
+				<div class='form-group'>
+					{{ Form::text('lastname','',array('class' => 'form-control input-md', 'placeholder' => 'Last Name', 'aria-describedby' => 'basic-addon1')) }}	
+					{{ $errors->first('lastname') }}
 				</div>
 
 				<div class='form-group'>
@@ -46,7 +64,9 @@
 			<div class='row'>
 
 				<div class='col-xs-12 col-md-6'>
-					{{ Form::submit('Create') }}
+					{{ Form::submit('Create', array('class' => 'btn btn-warning btn-block')) }}
+				</div>
+				</div>
 				</div>
 			</div>
 		{{ Form::close() }}
