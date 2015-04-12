cb83b24961175ff0e5b6867c679d326e0adf4a89
Ben <iglesia.ben@gmail.com>
2015-04-12 09:18:32 +0800

    Update Search result page.

--- app/views/page/searchresult.blade.php
+++ app/views/page/searchresult.blade.php
@@ -1,6 +1,56 @@
 @extends('layout.user_master')
 
 @section('contents')
-dito na
+<div class="container">
+<div class="col-lg-12">
+	<h1 class="page-header">Search Results</h1>
+</div>
 
+<div class="row">
+	<div class="col-lg-3 col-md-4 col-xs-6 thumb" id="itenerarylist">
+	    <a class="thumbnail" href="#">
+	        <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+	    </a>
+	</div>
+
+	<div class="col-lg-3 col-md-4 col-xs-6 thumb" id="itenerarylist">
+	    <a class="thumbnail" href="#">
+	        <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+	    </a>
+	</div>
+
+	<div class="col-lg-3 col-md-4 col-xs-6 thumb" id="itenerarylist">
+	    <a class="thumbnail" href="#">
+	        <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+	    </a>
+	</div>
+
+	<div class="col-lg-3 col-md-4 col-xs-6 thumb" id="itenerarylist">
+	    <a class="thumbnail" href="#">
+	        <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+	    </a>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-lg-3 col-md-4 col-xs-6 thumb" id="itenerarylist">
+	    <a class="thumbnail" href="#">
+	        <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+	    </a>
+	</div>
+
+	<div class="col-lg-3 col-md-4 col-xs-6 thumb" id="itenerarylist">
+	    <a class="thumbnail" href="#">
+	        <img class="img-responsive" src="http://placehold.it/400x300" alt="">
+	    </a>
+	</div>
+
+	<div class="col-lg-3 col-md-4 col-xs-6 thumb itemter">
+	    <a class="thumbnail" href="{{ URL::to('itenerary/create/' . $user->id) }}" id="additenerary">
+	        <span class="center glyphicon glyphicon-plus addter"></span>
+	    </a>
+	</div>
+</div>
+
+</div>
 @stop
