31484e2bbd589edc8c927f9722484ef8443d37fe
Ben <iglesia.ben@gmail.com>
2015-04-12 11:00:23 +0800

    Add fonts and update search bar.

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -5,40 +5,41 @@
 		{{ HTML::style('css/styles.css')}}
 	</head>
 		<body>
-	 	<nav class="navbar navbar-inverse navbar-fixed">
-	        <div class="container-fluid">
-	            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	                <ul class="nav navbar-nav navbar-left">
-	                 	<li class="active"><a href="{{ route('index') }}">WTH2015</a></li>
-	                	<li ><a href="{{ route('user.create') }}">Register</a></li>
-	                </ul>
-	                 <ul class="nav navbar-nav navbar-right">
-	                 {{ Form::open(['route' => 'search']) }}
-	            			
-            			<!-- <li>{{ Form::submit('Search', array('class' => 'btn btn-block btn-warning')) }}</li>::
-	            		<li>{{ Form::text('search', '', array('placeholder' => 'Search' , 'class' => 'form-control', 'aria-describedby' => 'basic-addon1')) }}</li>
-	            			{{ Form::close() }} -->
-	            		<li>
-						        <div class="col-md-10 col-md-offset-3">
-						            <form action="" class="search-form">
-						                <div class="form-group has-feedback">																																								
-						            		<label for="search" class="sr-only">Search</label>
-						            		<input type="text" class="form-control" name="search" id="search" placeholder="search">
-						              		<span class="glyphicon glyphicon-search form-control-feedback"></span>																																																														
-						            	</div>
-						            </form>
-						        </div>
-						</li>
-						{{ Form::close() }}
-	                	<li><a href="{{ route('login') }}">Login</a></li>
-	                </ul>
-	            </div>
-	        </div>
-	    </nav>
+		<nav class="navbar navbar-inverse">
+	  		<div class="container-fluid">
+
+		    <div class="navbar-header">
+		    	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        	<span class="sr-only">Toggle navigation</span>
+		        	<span class="icon-bar"></span>
+		        	<span class="icon-bar"></span>
+		        	<span class="icon-bar"></span>
+		      	</button>
+		    </div>
+
+	    	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      		<ul class="nav navbar-nav">
+	        		<li class="active"><a href="{{ route('index') }}">WTH2015</a></li>
+	        		<li ><a href="{{ route('user.create') }}">Register</a></li>
+	      		</ul>
+
+	      		<ul class="nav navbar-nav navbar-right">
+	        		<li>
+	            		{{ Form::open(['route' => 'search', 'class' => 'navbar-form navbar-left', 'role' => 'search']) }}
+	           			<div class="form-group">
+				        	<input type="text" class="form-control" placeholder="Search">
+				        </div>
+				        <button type="submit" class="btn btn-success">Submit</button>
+	           			{{ Form::close() }}
+	        		</li>
+	       			<li><a href="{{ route('login') }}">Login</a></li>
+	      		</ul>
+	    	</div>
+	  		</div>
+		</nav>
 	@yield('contents')
 
 
 		{{ HTML::script('js/jquery.min.js') }}
 		{{ HTML::script('js/bootstrap.min.js') }}
 		{{ HTML::script('js/script.js') }}
-
